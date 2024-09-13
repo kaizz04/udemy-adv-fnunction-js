7abd3bf3bc64d4cd3406d6155e00a36ded375945
kaizz04 <kaveesh@marketpushapps.com>
2024-09-13 12:27:42 +0530

    factory function

---
+++ app.js
@@ -0,0 +1,15 @@
+// function add(num1, num2){return num1+Math.random()};
+
+// console.log(add(9));
+
+function createTaxCalculator(tax){
+    return function calculateTax(amount){
+        return amount*tax;
+
+    }
+
+}
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+console.log(calculateVatAmount(2000));
+console.log(calculateIncomeTaxAmount(2000));
