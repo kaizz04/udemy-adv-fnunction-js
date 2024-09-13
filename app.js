a3bdb8326746184349ecf332a46129201ed849fc
kaizz04 <kaveesh@marketpushapps.com>
2024-09-13 12:58:09 +0530

    Closure

--- app.js
+++ app.js
@@ -11,5 +11,16 @@ function createTaxCalculator(tax){
 }
 const calculateVatAmount = createTaxCalculator(0.19);
 const calculateIncomeTaxAmount = createTaxCalculator(0.25);
-console.log(calculateVatAmount(2000));
-console.log(calculateIncomeTaxAmount(2000));
+// console.log(calculateVatAmount(2000));
+// console.log(calculateIncomeTaxAmount(2000));
+
+let userName = 'Max';
+function greetUser(){
+    //let name = manu;
+    console.log('Hi '+name);
+}
+
+let name = "kaizz";
+
+greetUser();
+
